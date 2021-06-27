371766f4f1f75be861698f7e7d7e5eedb4fd6004
ssamt <73675935+ssamt@users.noreply.github.com>
2021-06-27 21:27:43 +0900

    fixed last comic

--- smbc/urls.py
+++ smbc/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import smbc_view
 
 urlpatterns = [
-    path('', smbc_view),
+    path('', smbc_view, name='main'),
     path('<str:name>', smbc_view, name='comic'),
 ]
